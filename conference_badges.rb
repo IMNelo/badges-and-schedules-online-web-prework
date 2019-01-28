64eea2c5c7b4a6f78a9db4a72330b47140d069a4
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-01-28 20:26:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.insert(name, room)
+end
+
+def assign_rooms(array)
+  array = name
+  array.each_with_index {|name|, puts "Hello #{name}! You'll be assigned to room #{room}!"}
+end
